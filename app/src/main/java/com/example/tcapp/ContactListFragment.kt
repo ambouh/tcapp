6c7ba10e7a2067b2c859be400cb3be5ff7742f95
Andres Mbouh <andresmbouh@gmail.com>
2019-09-19 15:12:13 -0400

    modified ContactDetails to work

--- app/src/main/java/com/example/tcapp/ContactListFragment.kt
+++ app/src/main/java/com/example/tcapp/ContactListFragment.kt
@@ -1,7 +1,9 @@
 package com.example.tcapp
 
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -35,20 +37,25 @@ class ContactListFragment: Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val rootView = inflater.inflate(R.layout.contact_list_fragment, container,false)
         val fragmentActivity = rootView.context
-        /* val goToContactDetails = rootView.findViewById(R.id.goToContactDetails) as Button
+        val listContacts: ListView = rootView.findViewById(R.id.listContacts)
 
-        goToContactDetails.setOnClickListener{
-            showOtherFragment()
-        }*/
+        listContacts.adapter = ArrayAdapter(fragmentActivity, android.R.layout.simple_list_item_1, DataManager.contacts)
 
-        val listNotes: ListView = rootView.findViewById(R.id.listContacts)
-        listNotes.adapter = ArrayAdapter(fragmentActivity, android.R.layout.simple_list_item_1, DataManager.contacts)
+        listContacts.setOnItemClickListener{parent, view, position, id ->
+            showOtherFragment(position)
+        }
 
         return rootView
     }
 
-    private fun showOtherFragment() {
+    private fun showOtherFragment(contactPosition: Int) {
         val fr = ContactDetailsFragment.newInstance()
+        val bundle = Bundle()
+
+        bundle.putInt(EXTRA_CONTACT_POSITION, contactPosition)
+        Log.d("AAA", bundle.getInt(EXTRA_CONTACT_POSITION).toString())
+        fr.arguments = bundle
+
         val fc = btnListener
         fc.replaceFragment(fr)
     }
